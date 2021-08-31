a5bd93d79aca67b46549ff755d676f5d20fa1cc1
张宇涵王 <imwyh@foxmail.com>
2021-08-31 17:12:16 +0800

    upload base files

---
+++ include/bd.js
@@ -0,0 +1,128 @@
+var bd = {
+	"search": [{
+			"name": "谷歌",
+			"action": "https://www.google.com/search?q=",
+			"website": "https://www.google.com/"
+		},{
+			"name": "哔哩哔哩",
+			"action": "https://search.bilibili.com/all?keyword=",
+			"website": "https://search.bilibili.com/"
+		},{
+			"name": "淘宝",
+			"action": "https://s.taobao.com/search?q=",
+			"website": "https://s.taobao.com/"
+		},{
+			"name": "狗东",
+			"action": "https://search.jd.com/Search?keyword=",
+			"website": "https://jd.com/"
+		},
+		{
+			"name": "百度",
+			"action": "https://www.baidu.com/s?wd=",
+			"website": "https://www.baidu.com/"
+		},
+		{
+			"name": "duck",
+			"action": "https://duckduckgo.com/?q=",
+			"website": "https://duckduckgo.com/"
+		}
+	],
+	"link": [{
+			"pagination": "社交媒体",
+			"contain": [{
+					"name": "豆瓣",
+					"link": "https://www.douban.com",
+					"icon": "",
+					"span": "豆"
+				},
+				{
+					"name": "微博",
+					"link": "https://weibo.com/",
+					"icon": "sina-weibo",
+					"span": ""
+				},
+				{
+					"name": "微信",
+					"link": "https://wx.qq.com/",
+					"icon": "wechat",
+					"span": ""
+				},
+				{
+					"name": "知乎",
+					"link": "https://www.zhihu.com/",
+					"icon": "",
+					"span": "知"
+				},
+				{
+					"name": "贴吧",
+					"link": "https://tieba.baidu.com/index.html",
+					"icon": "",
+					"span": "du"
+				},
+				{
+					"name": "v2ex",
+					"link": "https://www.v2ex.com/",
+					"icon": "",
+					"span": "V"
+				}
+
+			]
+		},
+		{
+			"pagination": "学习",
+			"contain": [{
+					"name": "少数派",
+					"link": "https://sspai.com/",
+					"icon": "pi",
+					"span": ""
+				},
+				{
+					"name": "github",
+					"link": "https://github.com/",
+					"icon": "github",
+					"span": ""
+				},
+				{
+					"name": "菜鸟",
+					"link": "https://www.runoob.com/",
+					"icon": "language-html5",
+					"span": ""
+				},
+				{
+					"name": "w3school",
+					"link": "https://www.w3school.com.cn/",
+					"icon": "",
+					"span": "W3"
+				}
+			]
+		},
+		{
+			"pagination": "视频",
+			"contain": [{
+					"name": "爱奇艺",
+					"link": "https://www.iqiyi.com/",
+					"icon": "",
+					"span": "QIY"
+				},
+				{
+					"name": "腾讯视频",
+					"link": "https://v.qq.com/",
+					"icon": "google-play",
+					"span": ""
+				},
+				{
+					"name": "优酷",
+					"link": "https://www.youku.com/",
+					"icon": "play-box",
+					"span": ""
+				},
+				{
+					"name": "CCTV",
+					"link": "https://tv.cctv.com/live/cctv1/?",
+					"icon": "cctv",
+					"span": ""
+				}
+			]
+		}
+	]
+};
