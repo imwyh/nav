74ff90999c94bc6366022d6b0f4313f13f48b27d
张宇涵王 <imwyh@foxmail.com>
2021-09-03 12:42:44 +0800

    Update bd.js

--- include/bd.js
+++ include/bd.js
@@ -8,8 +8,10 @@ var bd = {
 		{"name": "duck","action": "https://duckduckgo.com/?q=","website": "https://duckduckgo.com/"}
 	],
 	"link": [
-			{"pagination": "社交媒体","contain": 
-			 [
+		{
+			"pagination": "社交媒体",
+			"contain": 
+			[
 				{"name": "豆瓣","link": "https://www.douban.com","icon": "","span": "豆"},
 				{"name": "微博","link": "https://weibo.com/","icon": "sina-weibo","span": ""},
 				{"name": "微信","link": "https://wx.qq.com/","icon": "wechat","span": ""},
